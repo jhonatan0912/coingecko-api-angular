c59fdcc8a9432c866464e5932d9864ef66faf8e4
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2023-04-17 12:03:47 -0500

    feat: app added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Coin } from './models/Coin.interface';
+import { CoinsService } from './services/coins.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'coingecko-api';
+export class AppComponent implements OnInit {
+
+  coins: Coin[] = [];
+  toles: string[] = ['#', 'Coin', 'Price', 'Price Change', '24h Volume'];
+
+  constructor(private coinService: CoinsService) { }
+
+  ngOnInit() {
+    this.coinService.getCoinsRequest()
+      .subscribe(res => {
+        this.coins = res;
+      })
+  }
+
 }
